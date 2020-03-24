79bf553f88585cf6590c4c248c4393e97eab4ce6
Jason Nelson <jason@carbonmade.com>
2020-03-24 14:27:04 -0700

    Add IdentifierTests

---
+++ src/Carbon.Acme.Tests/Models/IdentifierTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+using Xunit;
+
+namespace Carbon.Acme.Tests
+{
+    public class IdentifierTests
+    {
+        [Fact]
+        public void Construct()
+        {
+            var identifier = new Identifier("dns", "abc.com");
+
+            Assert.Equal("dns", identifier.Type);
+            Assert.Equal("abc.com", identifier.Value);
+
+            Assert.Equal("dns", JsonSerializer.Deserialize <Identifier>(JsonSerializer.Serialize(identifier)).Type);
+        }
+    }
+}
