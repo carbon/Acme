efc6c252eff0db1379d955ef839ba3126b89bde6
Jason Nelson <jason@carbonmade.com>
2025-01-22 19:01:23 -0800

    Use X509CertificateLoader.LoadCertificate

--- src/Carbon.Acme.Tests/Models/CertificateTests.cs
+++ src/Carbon.Acme.Tests/Models/CertificateTests.cs
@@ -21,12 +21,15 @@ public partial class CertificateChainTests
 
         Assert.Equal(2, pems.Count);
 
-        Assert.Equal("CN=acmev2.wulf.eu.org", new X509Certificate2(pems[0]).Subject);
-        Assert.Equal("CN=Fake LE Intermediate X1", new X509Certificate2(pems[1]).Subject);
+        using var cert0 = X509CertificateLoader.LoadCertificate(pems[0]);
+        using var cert1 = X509CertificateLoader.LoadCertificate(pems[1]);
 
-        Assert.Equal("CB10335FBFBBFDFC359A76559B46F0A4BA221736", new X509Certificate2(pems[0]).Thumbprint);
-        Assert.Equal("4EEE7398C1A3DAF91DA16689DB8243927A271B9A", new X509Certificate2(pems[1]).Thumbprint);
+        Assert.Equal("CN=acmev2.wulf.eu.org", cert0.Subject);
+        Assert.Equal("CN=Fake LE Intermediate X1", cert1.Subject);
 
-        Assert.Equal(pems[0], new X509Certificate2(pems[0]).RawData);
+        Assert.Equal("CB10335FBFBBFDFC359A76559B46F0A4BA221736", cert0.Thumbprint);
+        Assert.Equal("4EEE7398C1A3DAF91DA16689DB8243927A271B9A", cert1.Thumbprint);
+
+        Assert.Equal(pems[0], cert0.RawData);
     }
 }
